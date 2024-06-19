1b702ece3f8b620afa2880679cd81f7b89bc02d4
eduhejband <eduardohejband@gmail.com>
2024-06-18 23:36:40 -0300

    Update

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,16 @@
+// metro.config.js
+const { getDefaultConfig } = require('expo/metro-config');
+
 module.exports = {
-    resolver: {
-      // Opções de resolução de módulos
-      sourceExts: ['jsx', 'js', 'json', 'ts', 'tsx'], // Extensões de arquivo para resolução
-    },
-  };
-  
+  resolver: {
+    // Extensões de arquivo para resolução
+    sourceExts: ['jsx', 'js', 'json', 'ts', 'tsx', 'ttf'],
+    // Adicione manipuladores para assets se necessário
+    assetExts: ['ttf', 'png', 'jpg'], // Incluir outras extensões de assets conforme necessário
+  },
+  transformer: {
+    // Configure manipuladores de transformer se necessário
+    assetPlugins: ['expo-asset/tools/hashAssetFiles'], // Necessário para manipular assets em Expo
+  },
+  ...getDefaultConfig(__dirname), // Inclui configurações padrão do Expo
+};
